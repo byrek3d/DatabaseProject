a809f1cdd16722916bb053e04520240949d87f2f
Georgios Fotiadis <georgios.fotiadis@epfl.ch>
2019-05-17 15:26:51 +0200

    implemented some queries

--- deliverable3.sql
+++ deliverable3.sql
@@ -1,47 +1,47 @@
-SELECT
-(
- (SELECT MAX(Score) FROM
-   (SELECT TOP 50 PERCENT Score FROM Posts ORDER BY Score) AS BottomHalf)
- +
- (SELECT MIN(Score) FROM
-   (SELECT TOP 50 PERCENT Score FROM Posts ORDER BY Score DESC) AS TopHalf)
-) / 2 AS Median
-
-SELE
-
-
-
-WITH RankedTable AS (
-    SELECT neigbourhood, rewiew, 
-        ROW_NUMBER() OVER (PARTITION BY neighbourhod ORDER BY score) AS rank,
-        COUNT(*) OVER (PARTITION BY neighbourhhod) AS number_total
-    FROM (SELECT L.neigbourhood, S.score
+--Query 2
+WITH RankedTable AS(
+    SELECT neighbourhood, review_scores_rating,
+        ROW_NUMBER() OVER (PARTITION BY neighbourhood ORDER BY review_scores_rating) AS rank,
+        COUNT(*) OVER (PARTITION BY neighbourhood) AS number_total
+    FROM (SELECT L.neighbourhood, S.review_scores_rating
 		  FROM Listing L, Score S
-		  WHERE L.id=S.id )
+		  WHERE L.id=S.id AND L.city = 'Madrid')
 )
-
-SELECT TOP(5) R.neigbourhood
+SELECT R.neighbourhood
 FROM RankedTable R
-WHERE R.rank = R.number_total / 2 + 1  #change it 
-ORDER BY R.rewiew DESC
+WHERE R.rank = R.number_total / 2 + 1
+ORDER BY R.review_scores_rating DESC LIMIT 5;
 
-SELECT host_id
+--Query 3
+SELECT L.host_id, H.host_name
+FROM Listing L, Host H
+WHERE L.host_id = H.host_id
+GROUP BY L.host_id
+HAVING COUNT(*) = (SELECT COUNT(L.id) as max_listings
 FROM Listing L
 GROUP BY host_id
-ORDER BY COUNT (L.id) DESC
+ORDER BY COUNT (L.id) DESC limit 1)
 
-SELECT AVG(L
-FROM Listing L, Available_at AV, Score S, verified_by V
-WHERE L.city='Berlin' AND L.beds >=2 AND S.scores_location >=8 AND S.id= L.id
-	  AND L.cancellation_policy = 'flexible' AND L.host_id=V.host_id 
+--Query 4
+SELECT AVG(AV.price)
+FROM Listing L, Available_at AV, Score S, Verified_by V
+WHERE L.city='Berlin' AND L.beds >= 2 AND S.review_scores_location >= 8 AND S.id= L.id
+	  AND L.cancellation_policy = 'flexible' AND L.host_id = V.host_id
 	  AND (V.host_verifications='government_id' OR V.host_verifications='offline_government_id')
-	  AND 't'=ALL(SELECT AV1.available
-		          FROM Available_at AV1 
-                  WHERE L.id=AV1.id AND AV1.date BETWEEN '2019-03-01' AND '2019-04-30' )
-	  
-				  
-				  
-				  
-SELECT 
-FROM Listing L
-WHERE 't'= 	  
+	  AND L.id=AV.id AND AV.date >= '2019-03-01' AND AV.date <= '2019-04-30' AND AV.available = 't';
+
+
+--Query 5 RIP
+
+--Query 6
+WITH COUNTER AS (
+SELECT COUNT(R.review_id) as lala, L.id, H.host_id, H.host_name
+FROM Reviewed R, LISTING L,Host H
+WHERE  H.host_id = L.host_id AND R.id = L.id
+GROUP BY L.id
+)
+SELECT COUNT(*)
+FROM COUNTER C
+GROUP BY C.host_id
+ORDER BY
+
